fe973c7fb4fc4cb8e9109e30bdd2a3e2d86af106
am-cid <mcidantonio@gmail.com>
2023-11-21 01:29:49 +0800

    implement deletion on list all view

--- app/Livewire/TrialBalance/ListTrialBalance.php
+++ app/Livewire/TrialBalance/ListTrialBalance.php
@@ -8,11 +8,27 @@ use Livewire\Component;
 class ListTrialBalance extends Component
 {
     public $trial_balances = [];
+    public $confirming = null;
 
     public function mount() {
         // TODO: Change to DB query
         $this->trial_balances = TrialBalance::all();
     }
+
+    public function confirmDelete($tbId)
+    {
+        $this->confirming = $tbId;
+    }
+
+    public function deleteTrialBalance($tbId)
+    {
+        // delete by ID ($tbId) and refresh
+        TrialBalance::find($tbId)->delete();
+        // TODO: Change to DB query
+        $this->trial_balances = TrialBalance::all();
+        $this->confirming = null;
+    }
+    
     public function render()
     {
         return view('livewire.trial-balance.list-trial-balance');
--- resources/views/livewire/trial-balance/list-trial-balance.blade.php
+++ resources/views/livewire/trial-balance/list-trial-balance.blade.php
@@ -5,6 +5,15 @@
             <a href="/trial-balances/{{ $tb->tb_id }}">{{ $tb->tb_name }}</a>
             <div>{{ $tb->period }}</div>
             <div>{{ $tb->tb_data }}</div>
+            <div>
+                <!-- delete -->
+                <button wire:click="confirmDelete('{{ $tb->tb_id }}')">Delete</button>
+                <!-- confirm deletion -->
+                @if ($confirming === $tb->tb_id)
+                    <button wire:click="deleteTrialBalance('{{ $tb->tb_id }}')">Confirm Delete</button>
+                    <button wire:click="$set('confirming', null)">Cancel</button>
+                @endif
+            </div>
         @endforeach
     </section>
     @endif
