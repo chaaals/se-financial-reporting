3f201a1e4fd4fe883efe85253d63017a4d58f498
am-cid <mcidantonio@gmail.com>
2023-11-23 01:31:32 +0800

    allow editing of some fields

--- app/Livewire/FinancialReport/PreviewFinancialReport.php
+++ app/Livewire/FinancialReport/PreviewFinancialReport.php
@@ -10,10 +10,17 @@ class PreviewFinancialReport extends Component
 {
     public $financial_report;
     public $confirming = null;
+    public $editMode = false;
+    public $editedReportName;
+    public $editedApproved;
+    public $editedReportStatus;
 
     public function mount(){
         $report_id = Route::current()->parameter("report_id");
         $this->financial_report = FinancialReport::find($report_id);
+        $this->editedReportName = $this->financial_report->report_name;
+        $this->editedApproved = $this->financial_report->approved;
+        $this->editedReportStatus = $this->financial_report->report_status;
     }
 
     public function confirmDelete($report_id)
@@ -23,12 +30,40 @@ class PreviewFinancialReport extends Component
 
     public function deleteFinancialReport($report_id)
     {
-        // Delete financial report by ID
+        // delete by ID
         FinancialReport::find($report_id)->delete();
-        
-        // Redirect after deletion
         $this->reset('confirming');
         $this->redirect("/financial-reports");
+    }
+
+    public function toggleEditMode()
+    {
+        $this->editMode = !$this->editMode;
+    }
+
+    public function updateFinancialReport()
+    {
+        // check if the report is already approved but changed to not approved
+        if ($this->financial_report->approved) {
+            if (!$this->editedApproved) {
+                $this->editedReportStatus = 'For Approval';
+            }
+        }
+
+        // if not approved in the first place but changed to not approved
+        if ($this->editedApproved) {
+            $this->editedReportStatus = 'Approved';
+        }
+        
+
+        // update fields
+        $this->financial_report->report_name = $this->editedReportName;
+        $this->financial_report->approved = $this->editedApproved;
+        $this->financial_report->report_status = $this->editedReportStatus;
+        $this->financial_report->save();
+
+        // exit edit mode
+        $this->editMode = false;
     }
 
     public function render()
--- resources/views/livewire/financial-report/preview-financial-report.blade.php
+++ resources/views/livewire/financial-report/preview-financial-report.blade.php
@@ -1,24 +1,52 @@
 <div>
     @if ($financial_report)
         <section>
-            <div>{{ $financial_report->report_name }}</div>
-            <div>{{ $financial_report->period }}</div>
-            <div>{{ $financial_report->start_date }}</div>
-            <div>{{ $financial_report->end_date }}</div>
-            <div>{{ $financial_report->report_type }}</div>
-            <div>{{ $financial_report->report_status }}</div>
-            <div>{{ $financial_report->approved }}</div>
-            <div>{{ $financial_report->tb_id }}</div>
+            @if ($editMode)
+                <div>
+                    <div>
+                        <label for="report_name">Report Name:</label>
+                        <input id="report_name" type="text" wire:model="editedReportName">
+                    </div>
+                    <div>
+                        <label for="approved">Approved:</label>
+                        <input id="approved" type="checkbox" wire:model="editedApproved" @if ($editedReportStatus === 'Draft') disabled @endif>
+                    </div>
+                    <div>
+                        <label for="report_status">Report Status:</label>
+                        @if (!$editedApproved)
+                            <select id="report_status" wire:model="editedReportStatus">
+                                @if (!$editedApproved)
+                                    <option value="Draft">Draft</option>
+                                    <option value="For Approval">For Approval</option>
+                                @endif
+                            </select>
+                        @else
+                            <input id="report_status" type="text" value="Approved" disabled>
+                        @endif
+                    </div>
+                    <button wire:click="updateFinancialReport">Save</button>
+                    <button wire:click="toggleEditMode">Cancel</button>
+                </div>
+            @else
+                    <div>Report Name: {{ $financial_report->report_name }}</div>
+                    <div>Start Date: {{ $financial_report->start_date }}</div>
+                    <div>End Date: {{ $financial_report->end_date }}</div>
+                    <div>Report Type: {{ $financial_report->report_type }}</div>
+                    <div>Report Status: {{ $financial_report->report_status }}</div>
+                    <div>Approved: {{ $financial_report->approved }}</div>
 
-            <div>
-                <!-- delete -->
-                <button wire:click="confirmDelete('{{ $financial_report->report_id }}')">Delete</button>
-                <!-- confirm deletion -->
-                @if ($confirming === $financial_report->report_id)
-                    <button wire:click="deleteFinancialReport('{{ $financial_report->report_id }}')">Confirm Delete</button>
-                    <button wire:click="$set('confirming', null)">Cancel</button>
-                @endif
-            </div>
+                    <div>
+                        <!-- edit button -->
+                        <button wire:click="toggleEditMode">Edit</button>
+                        <!-- delete -->
+                        <button wire:click="confirmDelete('{{ $financial_report->report_id }}')">Delete</button>
+                        <!-- confirm deletion -->
+                        @if ($confirming === $financial_report->report_id)
+                            <button wire:click="deleteFinancialReport('{{ $financial_report->report_id }}')">Confirm Delete</button>
+                            <button wire:click="$set('confirming', null)">Cancel</button>
+                        @endif
+                    </div>
+            @endif
         </section>
     @endif
 </div>
