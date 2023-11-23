7135fbc00cd95563498aadd72c23b571d4adb658
am-cid <mcidantonio@gmail.com>
2023-11-23 17:29:57 +0800

    revert allowing editing on list view

--- app/Livewire/FinancialReport/ListFinancialReport.php
+++ app/Livewire/FinancialReport/ListFinancialReport.php
@@ -3,32 +3,16 @@
 namespace App\Livewire\FinancialReport;
 
 use App\Models\FinancialReport;
-use App\Models\TrialBalance;
 use Livewire\Component;
 
 class ListFinancialReport extends Component
 {
     public $financial_reports = [];
-    public $trial_balances = [];
     public $confirming = null;
-    public $editMode = false;
-    public $editedReportID;
-    public $editedReportName;
-    public $editedApproved;
-    public $editedReportStatus;
-    public $editedTBID;
-
-    protected $rules = [
-        'editedReportName' => 'nullable|max:255',
-        'editedApproved' => 'required|boolean',
-        'editedReportStatus' => 'required|in:Draft,For Approval,Approved',
-        'editedTBID' => 'required',
-    ];
 
     public function mount() {
-        // TODO: Change to DB queries
+        // TODO: Change to DB query
         $this->financial_reports = FinancialReport::all();
-        $this->trial_balances = TrialBalance::all();
     }
 
     public function confirmDelete($report_id)
@@ -47,49 +31,6 @@ class ListFinancialReport extends Component
         $this->reset('confirming');
     }
 
-    public function toggleEditMode($report_id)
-    {
-        $this->editMode = true;
-        $this->editedReportID = $report_id;
-
-        $report = FinancialReport::find($report_id);
-        $this->editedReportName = $report->report_name;
-        $this->editedApproved = $report->approved;
-        $this->editedReportStatus = $report->report_status;
-        $this->editedTBID = $report->tb_id;
-    }
-
-    public function updateFinancialReport()
-    {
-        $this->validate();
-        $report = FinancialReport::find($this->editedReportID);
-
-        // check if the report is already approved but changed to not approved
-        if ($report->approved) {
-            if (!$this->editedApproved) {
-                $this->editedReportStatus = 'For Approval';
-            }
-        }
-
-        // if not approved in the first place but changed to not approved
-        if ($this->editedApproved) {
-            $this->editedReportStatus = 'Approved';
-        }
-
-        // Update financial report fields
-        $report->report_name = $this->editedReportName;
-        $report->approved = $this->editedApproved;
-        $report->report_status = $this->editedReportStatus;
-        $report->tb_id = $this->editedTBID;
-        $report->save();
-        
-        $this->editMode = false;
-        
-        // refresh only the edited report in the list view
-        $index = array_search($this->editedReportID, array_column($this->financial_reports->toArray(), 'report_id'));
-        $this->financial_reports[$index] = $report;
-    }
-    
     public function render()
     {
         return view('livewire.financial-report.list-financial-report');
--- resources/views/livewire/financial-report/list-financial-report.blade.php
+++ resources/views/livewire/financial-report/list-financial-report.blade.php
@@ -2,63 +2,23 @@
     @if($financial_reports)
     <section>
         @foreach($financial_reports as $fr)
-            @if ($editMode && $editedReportID === $fr->report_id)
-                <div>
-                    <label for="report_name">Report Name:</label>
-                    <input id="report_name" type="text" wire:model="editedReportName" placeholder="optional">
-                </div>
-                <div>
-                    <label for="tb_ids">Trial Balance:</label>
-                    @if ($trial_balances)
-                        <select id="tb_ids" wire:model="editedTBID">
-                            @foreach ($trial_balances as $tb)
-                                <option value="{{ $tb->tb_id }}">{{ $tb->tb_name }}</option>
-                            @endforeach
-                        </select>
-                    @else
-                        <input id="tb_ids" type="text" value="No Trial Balance Available" disabled>
-                    @endif
-                </div>
-                <div>
-                    <label for="approved">Approved:</label>
-                    <input id="approved" type="checkbox" wire:model="editedApproved" @if ($editedReportStatus === 'Draft') disabled @endif>
-                </div>
-                <div>
-                    <label for="report_status">Report Status:</label>
-                    @if (!$editedApproved)
-                        <select id="report_status" wire:model="editedReportStatus">
-                            @if (!$editedApproved)
-                                <option value="Draft">Draft</option>
-                                <option value="For Approval">For Approval</option>
-                            @endif
-                        </select>
-                    @else
-                        <input id="report_status" type="text" value="Approved" disabled>
-                    @endif
-                </div>
-                <button wire:click="updateFinancialReport">Save</button>
-                <button wire:click="$set('editMode', false)">Cancel</button>
-            @else
-                <a href="/financial-reports/{{ $fr->report_id }}">{{ $fr->report_name }}</a>
-                <div>{{ $fr->period }}</div>
-                <div>{{ $fr->start_date }}</div>
-                <div>{{ $fr->end_date }}</div>
-                <div>{{ $fr->report_type }}</div>
-                <div>{{ $fr->report_status }}</div>
-                <div>{{ $fr->approved }}</div>
-                <div>{{ $fr->tb_id }}</div>
-                <div>
-                    <!-- Edit button -->
-                    <button wire:click="toggleEditMode('{{ $fr->report_id }}')">Edit</button>
-                    <!-- delete -->
-                    <button wire:click="confirmDelete('{{ $fr->report_id }}')">Delete</button>
-                    <!-- confirm deletion -->
-                    @if ($confirming === $fr->report_id)
-                        <button wire:click="deleteFinancialReport('{{ $fr->report_id }}')">Confirm Delete</button>
-                        <button wire:click="$set('confirming', null)">Cancel</button>
-                    @endif
-                </div>
-            @endif
+            <a href="/financial-reports/{{ $fr->report_id }}">{{ $fr->report_name }}</a>
+            <div>{{ $fr->period }}</div>
+            <div>{{ $fr->start_date }}</div>
+            <div>{{ $fr->end_date }}</div>
+            <div>{{ $fr->report_type }}</div>
+            <div>{{ $fr->report_status }}</div>
+            <div>{{ $fr->approved }}</div>
+            <div>{{ $fr->tb_id }}</div>
+            <div>
+                <!-- delete -->
+                <button wire:click="confirmDelete('{{ $fr->report_id }}')">Delete</button>
+                <!-- confirm deletion -->
+                @if ($confirming === $fr->report_id)
+                    <button wire:click="deleteFinancialReport('{{ $fr->report_id }}')">Confirm Delete</button>
+                    <button wire:click="$set('confirming', null)">Cancel</button>
+                @endif
+            </div>
         @endforeach
     </section>
     @endif
