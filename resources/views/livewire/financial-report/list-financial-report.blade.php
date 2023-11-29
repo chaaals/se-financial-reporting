8632d076956a7007de7f601cf73ccf3ead5cc665
am-cid <mcidantonio@gmail.com>
2023-11-29 15:44:03 +0800

    change fr list view

--- resources/views/livewire/financial-report/list-financial-report.blade.php
+++ resources/views/livewire/financial-report/list-financial-report.blade.php
@@ -2,21 +2,20 @@
     @if($financial_reports)
     <section>
         @foreach($financial_reports as $fr)
-            <a href="/financial-reports/{{ $fr->report_id }}">{{ $fr->report_name }}</a>
-            <div>{{ $fr->period }}</div>
-            <div>{{ $fr->start_date }}</div>
-            <div>{{ $fr->end_date }}</div>
-            <div>{{ $fr->report_type }}</div>
-            <div>{{ $fr->report_status }}</div>
-            <div>{{ $fr->approved }}</div>
-            <div>{{ $fr->tb_id }}</div>
+            <a href="/financial-reports/{{ $fr->report_id }}">Report Name: {{ $fr->report_name }}</a>
+            <div>Fiscal Year: {{ $fr->fiscal_year }}</div>
+            <div>Interim Period: {{ $fr->interim_period }}</div>
+            <div>Quarter: {{ $fr->quarter }}</div>
+            <div>Report Status: {{ $fr->report_status }}</div>
+            <div>Approved: {{ $fr->approved }}</div>
+            <div>tb_id: {{ $fr->tb_id }}</div>
             <div>
                 <!-- delete -->
                 <button wire:click="confirmDelete('{{ $fr->report_id }}')">Delete</button>
                 <!-- confirm deletion -->
                 @if ($confirming === $fr->report_id)
-                    <button wire:click="deleteFinancialReport('{{ $fr->report_id }}')">Confirm Delete</button>
-                    <button wire:click="$set('confirming', null)">Cancel</button>
+                <button wire:click="deleteFinancialReport('{{ $fr->report_id }}')">Confirm Delete</button>
+                <button wire:click="$set('confirming', null)">Cancel</button>
                 @endif
             </div>
         @endforeach
