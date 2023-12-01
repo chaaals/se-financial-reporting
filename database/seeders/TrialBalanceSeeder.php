aa1f6eafd9a11b4a85457b79da4403b3948d5008
chaaals <charlesching0808@gmail.com>
2023-12-02 00:48:06 +0800

    chore: add trial balance db seeder

---
+++ database/seeders/TrialBalanceSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TrialBalance;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrialBalanceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TrialBalance::factory(10)->create([
+            'tb_data' => '{ data: sample data }',
+            'report_name' => 'Test TB report',
+            'report_status' => 'Draft',
+            'date' => date('Y-m-d'),
+            'interim_period' => 'Monthly',
+            'template_name' => 'tb'
+        ]);
+    }
+}
