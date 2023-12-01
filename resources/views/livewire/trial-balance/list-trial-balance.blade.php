b97afc9e59159accca19dc7d08fec6c828fbc7f3
chaaals <charlesching0808@gmail.com>
2023-12-01 22:36:01 +0800

    feat: add sort button on thead

--- resources/views/livewire/trial-balance/list-trial-balance.blade.php
+++ resources/views/livewire/trial-balance/list-trial-balance.blade.php
@@ -20,15 +20,40 @@
 
     <table class="w-full table-auto rounded-t-lg overflow-hidden">
         <thead class="bg-primary text-white">
-            <th class="text-left p-2">Report name</th>
-            <th class="hidden md:table-cell md:text-left p-2">Fiscal year</th>
-            <th class="hidden text-left md:table-cell p-2">Interim period</th>
-            <th class="hidden text-left md:table-cell p-2">Date</th>
-            <th class="text-left p-2">Report status</th>
+            <th class="relative text-left px-4 py-2">
+                Report name
+                <button class="absolute top-1 right-2">
+                    <x-financial-reporting.assets.table-sort />
+                </button>
+            </th>
+            <th class="relative hidden md:table-cell md:text-left p-2">
+                Report type
+                <button class="absolute top-1 right-2">
+                    <x-financial-reporting.assets.table-sort />
+                </button>
+            </th>
+            <th class="relative hidden text-left md:table-cell p-2">
+                Interim period
+                <button class="absolute top-1 right-2">
+                    <x-financial-reporting.assets.table-sort />
+                </button>
+            </th>
+            <th class="relative hidden text-left md:table-cell p-2">
+                Date
+                <button class="absolute top-1 right-2">
+                    <x-financial-reporting.assets.table-sort />
+                </button>
+            </th>
+            <th class="relative text-left p-2">
+                Report status
+                <button class="absolute top-1 right-2">
+                    <x-financial-reporting.assets.table-sort />
+                </button>
+            </th>
         </thead>
 
         <tbody>
-            
+        
         </tbody>
     </table>
 </section>
