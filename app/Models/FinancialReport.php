21ba03630c94164c52d9364f667b8d5abe21dd4f
am-cid <mcidantonio@gmail.com>
2023-12-01 14:09:19 +0800

    update fr model again

--- app/Models/FinancialReport.php
+++ app/Models/FinancialReport.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class FinancialReport extends Model
 {
-    protected $primaryKey = 'report_id';
     use HasFactory;
+    protected $primaryKey = 'report_id';
+    public $incrementing = false;
+
+    protected static function booted()
+    {
+        /**
+         * allow report_id to be read in creation so it can be used
+         * as a foreign key
+         */ 
+        static::creating(function ($model) {
+            $model->report_id = Str::uuid()->toString();
+        });
+    }
 
     protected $fillable = [
         'report_name',
