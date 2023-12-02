6ce2bb93cafa6fee62f238da566ceef614ec7c1b
am-cid <mcidantonio@gmail.com>
2023-12-02 22:41:04 +0800

    fs imports

--- app/Livewire/FinancialStatement/AddFinancialStatement.php
+++ app/Livewire/FinancialStatement/AddFinancialStatement.php
@@ -4,9 +4,12 @@ namespace App\Livewire\FinancialStatement;
 
 // use App\Imports\FinancialStatementImport;
 use App\Models\FinancialStatement;
+use App\Models\ReportTemplate;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Storage;
 
 class AddFinancialStatement extends Component
 {
@@ -14,14 +17,14 @@ class AddFinancialStatement extends Component
 
     public $fsName;
     public $fsType;
+    public $fsData;
     public $date;
     public $interimPeriod;
     public $quarter;
-    
     public $importedSpreadsheet;
-
     public $spreadsheet = [];
     public $preview = [];
+
     protected $rules = [
         "fsName" => "nullable|max:255",
         "date" => "required|date",
@@ -42,22 +45,24 @@ class AddFinancialStatement extends Component
     public function add(){
         $fr_month = date('m', strtotime($this->date));
 
-        if ($this->interimPeriod === 'Quarterly') {
-            $this->rules['quarter'] = 'required|in:Q1,Q2,Q3,Q4';
-            $quarter = ceil($fr_month / 3);
-            $this->quarter = "Q$quarter";
-            $this->fsName = "Q$quarter Financial Statement " . date('Y');
-        } else {
-            $this->quarter = null;
-            $this->fsName = "Annual Financial Statement " . date('Y');
+        if(!$this->fsName) {
+            if ($this->interimPeriod === 'Quarterly') {
+                $this->rules['quarter'] = 'required|in:Q1,Q2,Q3,Q4';
+                $quarter = ceil($fr_month / 3);
+                $this->quarter = "Q$quarter";
+                $this->fsName = "Q$quarter Financial Statement " . date('Y');
+            } else {
+                $this->quarter = null;
+                $this->fsName = "Annual Financial Statement " . date('Y');
+            }
         }
 
+        $this->processImportedSpreadsheet();
         $this->validate();
-        if($this->spreadsheet){
+        if($this->importedSpreadsheet){
             FinancialStatement::create([
                 "fs_type" => $this->fsType,
-                // "tb_data" => json_encode($this->spreadsheet),
-                "fs_data" => '{"sample":"content"}',
+                "fs_data" => $this->fsData,
                 "report_name" => $this->fsName,
                 "interim_period" => $this->interimPeriod,
                 "quarter" => $this->quarter,
@@ -71,21 +76,26 @@ class AddFinancialStatement extends Component
         $this->redirect('/financial-statements');
     }
 
-    // public function previewSpreadsheet(){
-    //     $path = $this->importedSpreadsheet->getRealPath();
+    public function processImportedSpreadsheet()
+    {
+        $uploadedFile = $this->importedSpreadsheet;
+        $path = Storage::disk('local')->putFileAs('uploads', $uploadedFile, $uploadedFile->getClientOriginalName());
+        $spreadsheet = IOFactory::load(storage_path('app/'.$path));
+        $sheet = $spreadsheet->getActiveSheet();
         
-    //     $this->spreadsheet = (new FinancialStatementImport)->toArray($path)[0];
-        
-    //     $this->preview["headers"] = array_slice($this->spreadsheet,5,1);
-    //     $this->preview["data"] = array_slice($this->spreadsheet,6);
-    // }
+        $templateName = strtolower($this->fsType) . '_vals';
+        $jsonMap = ReportTemplate::where('template_name', $templateName)->value('template');
+
+        $column = ($this->fsType === 'SCF') ? 'E' : 'F';
+        $rowValues = json_decode($jsonMap, true);
+        foreach ($rowValues as &$row) {
+            $row = $sheet->getCell($column . $row)->getValue() ?? 0;
+        }
+        $this->fsData = json_encode($rowValues);
+    }
 
     public function render()
     {
-        // if($this->importedSpreadsheet){
-        //     $this->previewSpreadsheet();
-        // }
-
         return view('livewire.financial-statement.add-financial-statement');
     }
 }
