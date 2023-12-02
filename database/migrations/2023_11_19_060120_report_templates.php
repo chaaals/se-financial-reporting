bfa9e3a80deaee55b22f2e330c06dae5138640b2
am-cid <mcidantonio@gmail.com>
2023-12-03 00:14:22 +0800

    update report_templates migration

--- database/migrations/2023_11_19_060120_report_templates.php
+++ database/migrations/2023_11_19_060120_report_templates.php
@@ -38,6 +38,18 @@ return new class extends Migration
                 'template_name' => 'scf',
                 'template' => '{"sample": "content"}',
             ],
+            [
+                'template_name' => 'sfpo_vals',
+                'template' => '[17,18,19,20,21,23,27,28,32,33,34,35,36,37,46,47,48,49,53,61,62,63,64]',
+            ],
+            [
+                'template_name' => 'sfpe_vals',
+                'template' => '[14,15,16,17,22,23,24,25,26,27]',
+            ],
+            [
+                'template_name' => 'scf_vals',
+                'template' => '[13,15,16,20,21,29,33,34,35]',
+            ],
         ]);
     }
 
