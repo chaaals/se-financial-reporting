a1822e3613b6c204c25e4952d6b5cd4110f751e3
am-cid <mcidantonio@gmail.com>
2023-12-04 14:53:54 +0800

    model: fs -> fs_collection

--- app/Models/FinancialStatementCollection.php
+++ app/Models/FinancialStatementCollection.php
@@ -5,19 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FinancialStatement extends Model
+class FinancialStatementCollection extends Model
 {
-    protected $primaryKey = 'statement_id';
+    protected $primaryKey = 'collection_id';
     use HasFactory;
     protected $fillable = [
         'fs_type',
         'fs_data',
-        'report_name',
-        'report_status',
+        'collection_name',
+        'collection_status',
         'quarter',
         'approved',
         'date',
         'interim_period',
+        'tb_id',
         'template_name',
     ];
     protected $casts = [
