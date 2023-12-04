32991e3c20fc3459b96f2f28eb46d82a0632c7b2
am-cid <mcidantonio@gmail.com>
2023-12-04 15:01:12 +0800

    fix: fs class name

--- app/Models/FinancialStatement.php
+++ app/Models/FinancialStatement.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FinancialStatementCollection extends Model
+class FinancialStatement extends Model
 {
     protected $primaryKey = 'fs_id';
     use HasFactory;
