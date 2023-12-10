33c256882478021c219693d08ffb548c8855a68c
chaaals <charlesching0808@gmail.com>
2023-12-10 23:00:57 +0800

    feat: init financial statements blade template

---
+++ app/Livewire/FinancialReporting/FinancialPerformanceTemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\FinancialReporting;
+
+use Livewire\Component;
+
+class FinancialPerformanceTemplate extends Component
+{
+    public function render()
+    {
+        return view('livewire.financial-reporting.financial-performance-template');
+    }
+}
---
+++ app/Livewire/FinancialReporting/FinancialPositionTemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\FinancialReporting;
+
+use Livewire\Component;
+
+class FinancialPositionTemplate extends Component
+{
+    public function render()
+    {
+        return view('livewire.financial-reporting.financial-position-template');
+    }
+}
