b0a9fc3574fd2cea7d84ce31b8ecbadc4b7cdc7a
am-cid <mcidantonio@gmail.com>
2024-03-30 23:11:45 +0800

    chore: add tb-data-history table to migrations to store `tb_data`
    remove `tb_data` in trial_balances table

---
+++ database/migrations/2023_11_19_060122_tb_data_history.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_data_history', function (Blueprint $table) {
+            $table->uuid('tb_data_id')
+                ->default(DB::raw('(UUID())'))
+                ->primary();
+            $table->foreignUuid('tb_id')
+                ->constrained(table: 'trial_balances', column: 'tb_id')
+                ->cascadeOnDelete();
+            $table->longText('tb_data'); // json
+            $table->date('tb_date');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trial_balances');
+    }
+};
+
