44edd8e4efe80816b6038759b4354a4b507f061a
chaaals <charlesching0808@gmail.com>
2024-04-12 03:51:39 +0800

    feat: add dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-general-layout pageTitle="Home">
-    <h1>Home</h1>
-    <div>{{ auth()->user() }}</div>
+    {{-- <h1>Home</h1>
+    <div>{{ auth()->user() }}</div> --}}
+    <livewire:home :user="auth()->user()"/>
 </x-general-layout>
