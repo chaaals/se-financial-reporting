9ec7c1aea8e3a91af82eab94eea0bcfb7b2c03d7
am-cid <mcidantonio@gmail.com>
2024-05-03 01:13:43 +0800

    feat: new fields for trial_balances table: credit and debit totals

--- database/migrations/2023_11_19_060121_trial_balances.php
+++ database/migrations/2023_11_19_060121_trial_balances.php
@@ -14,15 +14,17 @@ return new class extends Migration
         Schema::create('trial_balances', function (Blueprint $table) {
             $table->uuid('tb_id')
                 ->primary();
-            $table->enum('tb_type', ['pre','post'])->nullable();
+            $table->enum('tb_type', ['pre', 'post'])->nullable();
 
             $table->string('tb_name');
-            $table->enum('tb_status', ['Draft','For Approval', 'Change Requested', 'Approved'])->default('Draft');
+            $table->enum('tb_status', ['Draft', 'For Approval', 'Change Requested', 'Approved'])->default('Draft');
             $table->enum('quarter', ['Q1', 'Q2', 'Q3', 'Q4'])->nullable();
             $table->boolean('approved')->default(false);
             $table->date('tb_date');
             $table->enum('interim_period', ['Monthly', 'Quarterly', 'Annual']);
             $table->string('template_name')->default('tb');
+            $table->integer('debit_grand_totals');
+            $table->integer('credit_grand_totals');
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
 
