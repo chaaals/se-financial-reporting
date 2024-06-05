9029974f0f9326a7281c26a3f8a20d3c5a3d22e2
chaaals <charlesching0808@gmail.com>
2024-06-05 22:19:14 +0800

    feat: remove dashboard filters when showing activity and order activity by most recent

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -131,9 +131,9 @@ class Home extends Component
         $logsQuery = null;
         if(auth()->user()->role == 'accounting'){
             $user = auth()->user()->first_name . " " . auth()->user()->last_name;
-            $logsQuery = Activity::where('properties->role', auth()->user()->role)->where('properties->user', $user)->paginate(10);
+            $logsQuery = Activity::where('properties->role', auth()->user()->role)->where('properties->user', $user)->orderBy('created_at','desc')->paginate(10);
         } else {
-            $logsQuery = Activity::select('*')->paginate(10);
+            $logsQuery = Activity::select('*')->orderBy('created_at','desc')->paginate(10);
         }
 
         $this->logs = $logsQuery->items();
