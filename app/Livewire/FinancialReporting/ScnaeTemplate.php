90daf356a56db4b6b9753d9f90659b5796a75570
chaaals <charlesching0808@gmail.com>
2024-06-10 20:02:43 +0800

    feat: init scnae template

---
+++ app/Livewire/FinancialReporting/ScnaeTemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\FinancialReporting;
+
+use Livewire\Component;
+
+class ScnaeTemplate extends Component
+{
+    public function render()
+    {
+        return view('livewire.financial-reporting.scnae-template');
+    }
+}
---
+++ resources/views/livewire/financial-reporting/scnae-template.blade.php
@@ -0,0 +1,10 @@
+<section class="h-full overflow-hidden overflow-y-scroll scrollbar rounded-t-lg">
+    <table class="w-full">
+        <thead>
+            <th class="text-sm p-2 sticky top-0 text-white bg-primary">Account Titles</th>
+            <th class="text-sm p-2 sticky top-0 text-white bg-primary">Amount</th>
+        </thead>
+        <tbody>
+           
+    </table>
+</section>
